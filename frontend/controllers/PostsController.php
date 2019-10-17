7ff80c951610721810b783cbf165c37bef30674d
Alexandr13Iv <alexandr13iv@gmail.com>
2019-10-18 00:06:04 +0300

    Lesson 6 - Створення контролера

---
+++ frontend/controllers/PostsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\models\Posts;
+
+/**
+ * Site controller
+ */
+class PostsController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionPosts()
+    {
+        $posts = Posts::find()->all();
+        return $this->render('posts', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function actionPost()
+    {
+        $post = Posts::find()->where(['id' => Yii::$app->request()->get()['id']])->one();
+        return $this->render('post', [
+            'post' => $post
+        ]);
+    }
+    
+
+}
